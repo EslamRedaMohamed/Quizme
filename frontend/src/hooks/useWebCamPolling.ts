742ffb0fe94fd71b0a19ece5144eddaa390e948a
Ahmed El-Kilani <ahmedelkilani01@gmail.com>
2024-10-01 09:07:19 +0300

    Webcam Polling: - added useWebcamPolling hook to the wrapper - fixed content type for axios instance so it properly sends blob bodies - retrieve attempt id from session storage to send as query param with blob frame - some other tweaks

--- frontend/src/hooks/useWebCamPolling.ts
+++ frontend/src/hooks/useWebCamPolling.ts
@@ -3,24 +3,33 @@ import postData from "../utils/postData";
 
 const FRAME_POLLING_RATE = 10000;
 const JPEG_IMAGE_QUALITY = 0.8;
-const useWebCamPolling = (stream: MediaStream) => {
+const useWebCamPolling = (stream: MediaStream | null) => {
   useEffect(() => {
+    // if (!stream) {
+    //   return;
+    // }
+    // Create video element and attach it to dom hidden
     const videoElement = document.createElement("video");
     videoElement.srcObject = stream;
+    videoElement.style.display = "none";
+    document.body.appendChild(videoElement);
     videoElement.play();
 
+    // Canvas for manipulating frames
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
     canvas.width = 640;
     canvas.height = 480;
-
+    console.log({ context });
     if (!context) {
       console.error("Failed to get canvas context for webcam polling");
       return;
     }
 
+    // Poll for frames with FRAME_POLLING_RATE
     const interval = setInterval(() => {
       context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+      // Convert canvas to blob then post it to server
       canvas.toBlob(
         (blob) => {
           if (!blob) {
@@ -29,9 +38,16 @@ const useWebCamPolling = (stream: MediaStream) => {
             );
             return;
           }
-          postData("/monitor-frame/", blob).catch((error) => {
-            console.error("Error sending frame:", error);
-          });
+          const attempt_id = sessionStorage.getItem("attempt_id");
+          if (!attempt_id) {
+            console.error("Attempt id not found for webcam polling");
+            return;
+          }
+          postData(`/monitor-frame?attempt_id=${attempt_id}`, blob).catch(
+            (error) => {
+              console.error("Error sending frame:", error);
+            }
+          );
         },
         "image/jpeg",
         JPEG_IMAGE_QUALITY
